1dd907329112be03157ea47ae750786fa439e7e0
vlada <vladartemova@gmail.com>
2021-04-18 17:39:47 +0500

    Initial

---
+++ SharpForms/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace SharpForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelText = new System.Windows.Forms.Label();
+            this.labelMark = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxMark = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMark)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 77);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Начать игру";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelText
+            // 
+            this.labelText.AutoSize = true;
+            this.labelText.Location = new System.Drawing.Point(31, 137);
+            this.labelText.Name = "labelText";
+            this.labelText.Size = new System.Drawing.Size(91, 15);
+            this.labelText.TabIndex = 1;
+            this.labelText.Text = "Начните игру...";
+            // 
+            // labelMark
+            // 
+            this.labelMark.AutoSize = true;
+            this.labelMark.Location = new System.Drawing.Point(655, 241);
+            this.labelMark.Name = "labelMark";
+            this.labelMark.Size = new System.Drawing.Size(75, 15);
+            this.labelMark.TabIndex = 4;
+            this.labelMark.Text = "Твоя оценка";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(31, 179);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(110, 110);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBoxMark
+            // 
+            this.pictureBoxMark.Location = new System.Drawing.Point(606, 273);
+            this.pictureBoxMark.Name = "pictureBoxMark";
+            this.pictureBoxMark.Size = new System.Drawing.Size(172, 156);
+            this.pictureBoxMark.TabIndex = 6;
+            this.pictureBoxMark.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(147, 179);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(110, 110);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(263, 179);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(110, 110);
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(31, 295);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(110, 110);
+            this.pictureBox4.TabIndex = 9;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(147, 295);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(110, 110);
+            this.pictureBox5.TabIndex = 10;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(263, 295);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(110, 110);
+            this.pictureBox6.TabIndex = 11;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBoxMark);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelMark);
+            this.Controls.Add(this.labelText);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMark)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelText;
+        private System.Windows.Forms.Label labelMark;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBoxMark;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+    }
+}
+
---
+++ SharpForms/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SharpForms
+{
+    public partial class Form1 : Form
+    {
+        private List<GameColor> colors = new List<GameColor>();
+        private SelectedGameColor[] randomColors = new SelectedGameColor[6];
+        private GameColor requiredColor;
+        private GameColor defaultColor = new GameColor(0, "", Color.Gray);
+        private Random random = new Random();
+        private int errorCounter = 0;
+        private bool isGameFinished = false;
+        private List<PictureBox> pictureBoxes = new List<PictureBox>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            initializeList();
+            initializePictureBoxes();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            initializeGame();
+        }
+
+        private void initializePictureBoxes()
+        {
+            pictureBoxes.Add(pictureBox1);
+            pictureBoxes.Add(pictureBox2);
+            pictureBoxes.Add(pictureBox3);
+            pictureBoxes.Add(pictureBox4);
+            pictureBoxes.Add(pictureBox5);
+            pictureBoxes.Add(pictureBox6);
+        }
+
+        private void initializeList()
+        {
+            GameColor red = new GameColor(1, "красного", Color.Red);
+            GameColor green = new GameColor(2, "зеленого", Color.Green);
+            GameColor blue = new GameColor(3, "синего", Color.Blue);
+            colors.Add(red);
+            colors.Add(green);
+            colors.Add(blue);
+        }
+
+        private void initializeGame()
+        {
+            errorCounter = 0;
+            isGameFinished = false;
+            setPictureBoxVisible();
+            createRandomColors();
+            fillPictureBoxes();
+            labelText.Text = createNewText();
+        }
+
+        private void fillPictureBoxes()
+        {
+            for (int i = 0; i < randomColors.Length; i++)
+            {
+                pictureBoxes[i].BackColor = randomColors[i].Color.Color;
+            }
+        }
+
+        private void createRandomColors()
+        {
+            requiredColor = getGameColorById(random.Next(1, 4));
+            for (int i = 0; i < randomColors.Length; i++)
+            {
+                int randomNumber = random.Next(1, 4);
+                GameColor colorByRandom = getGameColorById(randomNumber);
+                randomColors[i] = new SelectedGameColor(colorByRandom);
+            }
+            if (!checkRandomColorsContainsRequired())
+            {
+                createRandomColors();
+            }
+        }
+
+        private bool checkRandomColorsContainsRequired()
+        {
+            GameColor[] mappedColors = randomColors.Select(color => color.Color).ToArray();
+            return mappedColors.Contains(requiredColor);
+        }
+
+        private GameColor getGameColorById(int id)
+        {
+            for (int i = 0; i < colors.Count; i++)
+            {
+                if (colors[i].isGameColorHasId(id))
+                {
+                    return colors[i];
+                }
+            }
+            return defaultColor;
+        }
+
+        private string createNewText()
+        {
+            return String.Concat("Найдите все фигуры ", requiredColor.Name, " цвета");
+        }
+
+        private void setPictureBoxVisible()
+        {
+            pictureBoxes.ForEach(pictureBox => pictureBox.Visible = true);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            handlePickboxClick(0);
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            handlePickboxClick(1);
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            handlePickboxClick(2);
+        }
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            handlePickboxClick(3);
+
+        }
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            handlePickboxClick(4);
+        }
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            handlePickboxClick(5);
+        }
+
+        private void handlePickboxClick(int boxIndex)
+        {
+            if (isGameFinished)
+            {
+                return;
+            }
+            pictureBoxes[boxIndex].Visible = false;
+            randomColors[boxIndex].IsSelected = true;
+            if (randomColors[boxIndex].Color != requiredColor)
+            {
+                errorCounter++;
+            }
+            isGameFinished = isAllCorrectPicturesAreSelected();
+            if (isGameFinished)
+            {
+                handleGameEnded();
+            }
+        }
+
+        private void handleGameEnded()
+        {
+            fillBoxesToDefault();
+            switch(errorCounter)
+            {
+                case 0:
+                    labelMark.Text = "Отлично";
+                    break;
+                case 1:
+                    labelMark.Text = "Хорошо";
+                    break;
+                case 2:
+                    labelMark.Text = "Удовлетворительно";
+                    break;
+                default:
+                    labelMark.Text = "Плохо";
+                    break;
+            }
+        }
+
+        private void fillBoxesToDefault()
+        {
+            pictureBoxes.ForEach(pictureBox => pictureBox.BackColor = defaultColor.Color);
+        }
+
+        private bool isAllCorrectPicturesAreSelected()
+        {
+            return randomColors
+                .Where(color => color.Color == requiredColor)
+                .All(color => color.IsSelected);
+        }
+
+        public class SelectedGameColor
+        {
+            private GameColor color;
+            private bool isSelected = false;
+
+            public SelectedGameColor(GameColor color)
+            {
+                this.Color = color;
+            }
+
+            public GameColor Color { get => color; set => color = value; }
+            public bool IsSelected { get => isSelected; set => isSelected = value; }
+        }
+
+        public class GameColor
+        {
+            private int id;
+            private string name;
+            private Color color;
+
+            public GameColor(int id, string name, Color color)
+            {
+                this.Id = id;
+                this.Name = name;
+                this.Color = color;
+            }
+
+            public int Id { get => id; set => id = value; }
+            public string Name { get => name; set => name = value; }
+            public Color Color { get => color; set => color = value; }
+
+            public override bool Equals(object obj)
+            {
+                return obj is GameColor color &&
+                       id == color.id &&
+                       name == color.name &&
+                       this.color.Equals(color.color) &&
+                       Id == color.Id &&
+                       Name == color.Name &&
+                       Color.Equals(color.Color);
+            }
+
+            public override int GetHashCode()
+            {
+                return HashCode.Combine(id, name, color, Id, Name, Color);
+            }
+
+            public bool isGameColorHasId(int id)
+            {
+                return this.Id == id;
+            }
+        }
+
+
+    }
+}
